adabb30821c8ab1de6723110e655f531a5254fd4
Raimond Spekking <raymond@users.mediawiki.org>
2007-09-08 04:39:31 +0000

    * (bug 11230) Update Chinese translations   Patches by Shinjiman

--- PdfHandler.i18n.php
+++ PdfHandler.i18n.php
@@ -9,18 +9,40 @@ function efPdfHandlerMessages() {
 		'en' => array(
 			'pdf_no_xml' => 'cannot get metadata from pdf',
 			'pdf_page_error' => 'page number not in range',
-	),
+		),
 
 		'de' => array(
 			'pdf_no_xml' => 'Keine Metadaten im PDF vorhanden.',
 			'pdf_page_error' => 'Seitenzahl außerhalb des Dokumentes.',
-	),
+		),
 
 		'nl' => array(
 			'pdf_no_xml' => 'de metadata van de PDF kan niet uitgelezen worden',
 			'pdf_page_error' => 'paginanummer komt niet voor in document',
-	),
-);
+		),
+
+		'yue' => array(
+			'pdf_no_xml' => '唔能夠響PDF度拎metadata',
+			'pdf_page_error' => '頁數唔響範圍度',
+		),
+
+		'zh-hans' => array(
+			'pdf_no_xml' => '无法在PDF中撷取元数据',
+			'pdf_page_error' => '页数不在范围中',
+		),
+
+		'zh-hant' => array(
+			'pdf_no_xml' => '無法在PDF中擷取元數據',
+			'pdf_page_error' => '頁數不在範圍中',
+		),
+	);
+
+	$messages['zh'] = $messages['zh-hans'];
+	$messages['zh-cn'] = $messages['zh-hans'];
+	$messages['zh-hk'] = $messages['zh-hant'];
+	$messages['zh-sg'] = $messages['zh-hans'];
+	$messages['zh-tw'] = $messages['zh-hant'];
+	$messages['zh-yue'] = $messages['yue'];
 
 	return $messages;
 }
