67eec437349f1b38164b77a6f3dc5144c2d75837
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-17 00:40:51 +0000

    Localisation updates for extension messages from Betawiki * added Kotava/avk * various formatting fixes

--- PdfHandler.i18n.php
+++ PdfHandler.i18n.php
@@ -108,7 +108,6 @@ $messages['zh-hant'] = array(
 	'pdf_page_error' => '頁數不在範圍中',
 );
 
-# Fallbacks:
 $messages['de-formal'] = $messages['de'];
 $messages['zh'] = $messages['zh-hans'];
 $messages['zh-cn'] = $messages['zh-hans'];
