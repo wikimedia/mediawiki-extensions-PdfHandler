7a38c56823406101272219719ad1c26c78142b5a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-04 07:35:43 +0000

    Rename non-English messages, capitalise PDF

--- PdfHandler.i18n.php
+++ PdfHandler.i18n.php
@@ -13,7 +13,7 @@ $messages['en'] = array(
 );
 
 $messages['ar'] = array(
-	'pdf_no_xml' => 'لم يمكن أخذ معلومات ميتا من pdf',
+	'pdf_no_metadata' => 'لم يمكن أخذ معلومات ميتا من PDF',
 	'pdf_page_error' => 'رقم الصفحة ليس في النطاق',
 );
 
@@ -21,26 +21,26 @@ $messages['ar'] = array(
  * @author DCLXVI
  */
 $messages['bg'] = array(
-	'pdf_no_xml' => 'невъзможно е да бъдат извлечени метаданни от pdf',
+	'pdf_no_metadata' => 'невъзможно е да бъдат извлечени метаданни от PDF',
 );
 
 $messages['de'] = array(
-	'pdf_no_xml' => 'Keine Metadaten im PDF vorhanden.',
+	'pdf_no_metadata' => 'Keine Metadaten im PDF vorhanden.',
 	'pdf_page_error' => 'Seitenzahl außerhalb des Dokumentes.',
 );
 
 $messages['fr'] = array(
-	'pdf_no_xml' => 'Ne peut obtenir les métadonnées du fichier PDF',
+	'pdf_no_metadata' => 'Ne peut obtenir les métadonnées du fichier PDF',
 	'pdf_page_error' => 'Le numéro de page n\'est pas dans l\'étendue.',
 );
 
 $messages['gl'] = array(
-	'pdf_no_xml' => 'non se puideron obter os metadatos do pdf',
+	'pdf_no_metadata' => 'non se puideron obter os metadatos do PDF',
 	'pdf_page_error' => 'o número de páxina non está no rango',
 );
 
 $messages['hsb'] = array(
-	'pdf_no_xml' => 'W pdf žane metadaty njejsu.',
+	'pdf_no_metadata' => 'W PDF žane metadaty njejsu.',
 	'pdf_page_error' => 'Ličba strony zwonka dokumenta.',
 );
 
@@ -48,7 +48,7 @@ $messages['hsb'] = array(
  * @author Bdanee
  */
 $messages['hu'] = array(
-	'pdf_no_xml'     => 'nem sikerült lekérni a pdf metaadatait',
+	'pdf_no_metadata'     => 'nem sikerült lekérni a PDF metaadatait',
 	'pdf_page_error' => 'az oldalszám a tartományon kívül esik',
 );
 
@@ -56,17 +56,17 @@ $messages['hu'] = array(
  * @author Robby
  */
 $messages['lb'] = array(
-	'pdf_no_xml'     => 'Meta-Informatiounen aus dem PDF Dokument kënne net gelies ginn',
+	'pdf_no_metadata'     => 'Meta-Informatiounen aus dem PDF Dokument kënne net gelies ginn',
 	'pdf_page_error' => 'Déi Säitenzuel gëtt et an dem Dokument net.',
 );
 
 $messages['nl'] = array(
-	'pdf_no_xml' => 'de metadata van de PDF kan niet uitgelezen worden',
+	'pdf_no_metadata' => 'de metadata van de PDF kan niet uitgelezen worden',
 	'pdf_page_error' => 'paginanummer komt niet voor in document',
 );
 
 $messages['no'] = array(
-	'pdf_no_xml' => 'kan ikke hente metadata fra PDF',
+	'pdf_no_metadata' => 'kan ikke hente metadata fra PDF',
 	'pdf_page_error' => 'sidenummer ikke innen rekkevidde',
 );
 
@@ -74,22 +74,22 @@ $messages['no'] = array(
  * @author Cedric31
  */
 $messages['oc'] = array(
-	'pdf_no_xml'     => 'Pòt pas obténer las metadonadas del fichièr PDF',
+	'pdf_no_metadata'     => 'Pòt pas obténer las metadonadas del fichièr PDF',
 	'pdf_page_error' => "Lo numèro de pagina es pas dins l'espandida.",
 );
 
 $messages['pl'] = array(
-	'pdf_no_xml' => 'nie można pobrać metadanych z pliku PDF',
+	'pdf_no_metadata' => 'nie można pobrać metadanych z pliku PDF',
 	'pdf_page_error' => 'numer strony poza zakresem',
 );
 
 $messages['pms'] = array(
-	'pdf_no_xml' => 'as peulo nen pijesse ij metadat dal pdf',
+	'pdf_no_metadata' => 'as peulo nen pijesse ij metadat dal PDF',
 	'pdf_page_error' => 'ël nùmer ëd pàgina a resta fòra da cole ch\'a-i son',
 );
 
 $messages['pt'] = array(
-	'pdf_no_xml' => 'não foi possível obter os metadados do pdf',
+	'pdf_no_metadata' => 'não foi possível obter os metadados do PDF',
 	'pdf_page_error' => 'página indisponível neste intervalo',
 );
 
@@ -97,7 +97,7 @@ $messages['pt'] = array(
  * @author .:Ajvol:.
  */
 $messages['ru'] = array(
-	'pdf_no_xml'     => 'невозможно получить метаданные из PDF',
+	'pdf_no_metadata'     => 'невозможно получить метаданные из PDF',
 	'pdf_page_error' => 'номер страницы вне диапазона',
 );
 
@@ -105,7 +105,7 @@ $messages['ru'] = array(
  * @author Helix84
  */
 $messages['sk'] = array(
-	'pdf_no_xml'     => 'nie je možné získať metadáta z pdf',
+	'pdf_no_metadata'     => 'nie je možné získať metadáta z PDF',
 	'pdf_page_error' => 'číslo stránky nie je v intervale',
 );
 
@@ -113,22 +113,22 @@ $messages['sk'] = array(
  * @author Pyt
  */
 $messages['stq'] = array(
-	'pdf_no_xml'     => 'Neen Metadoaten in dät PDF deer.',
+	'pdf_no_metadata'     => 'Neen Metadoaten in dät PDF deer.',
 	'pdf_page_error' => 'Siedentaal buute dät Dokument.',
 );
 
 $messages['yue'] = array(
-	'pdf_no_xml' => '唔能夠響PDF度拎metadata',
+	'pdf_no_metadata' => '唔能夠響PDF度拎metadata',
 	'pdf_page_error' => '頁數唔響範圍度',
 );
 
 $messages['zh-hans'] = array(
-	'pdf_no_xml' => '无法在PDF中撷取元数据',
+	'pdf_no_metadata' => '无法在PDF中撷取元数据',
 	'pdf_page_error' => '页数不在范围中',
 );
 
 $messages['zh-hant'] = array(
-	'pdf_no_xml' => '無法在PDF中擷取元數據',
+	'pdf_no_metadata' => '無法在PDF中擷取元數據',
 	'pdf_page_error' => '頁數不在範圍中',
 );
 
